3f15ba49d513f230996028f198bf8177ccc209ce
Tigran Mkrtchyan <TigranMkrtchyan.07.06@gmail.com>
2021-01-27 07:46:52 +0400

    was add isCycle function

--- lesson1/linkedList/index.js
+++ lesson1/linkedList/index.js
@@ -8,6 +8,12 @@ function createNode(data) {
 function createLinkedList() {
     let first = null, tail = null;
     return {
+        getHead: ()=>{
+            return first;
+        },
+        getTail:()=>{
+            return tail;
+        },
         add: (value) => {
             let newNode = createNode(value);
             if (first == null) {
@@ -21,22 +27,55 @@ function createLinkedList() {
         remove: () => {
             if (first == null) {
                 throw new Error("there is no element")
-            }else{
-                let tamp  = first;
+            } else {
+                let tamp = first;
                 first = first.next;
                 return tamp;
+            }
+        },
+        find: (val) => {
+            if (first == null) {
+                return null;
+            }
+            let current = first;
+            while (current != null) {
+                if (current.value === val) {
+                    return current;
+                }
+                current = current.next;
             }
         }
     }
 }
 
+function isCyclic(node) {
+    if (node == null) {
+        return false;
+    }
+    let first = node;
+    let second = node;
+    while (second.next != null) {
+        first = first.next;
+        second = second.next.next;
+        if (first === second) {
+            return true
+        }
+    }
+    return false;
+}
+
 let linkedList = createLinkedList();
 linkedList.add(12);
 linkedList.add(21);
-console.log(linkedList.remove().value);
-
 linkedList.add(23);
+console.log(linkedList.remove().value);
 linkedList.add(34);
-console.log(linkedList.remove().value);
-console.log(linkedList.remove().value);
-console.log(linkedList.remove().value);
+linkedList.add(42);
+linkedList.add(53);
+
+let node = linkedList.find(21);
+let tail = linkedList.getTail();
+tail.next = node;
+
+ console.log(isCyclic(linkedList.getHead()));
+
